d719d3d1229b36b326a3a2fe5a3b69521e1fceda
Jiwoo <blanketfys@gmail.com>
2026-05-29 21:31:33 +0700

    fix login autofill validation issue

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,6 +6,10 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "new" do
     get new_session_path
     assert_response :success
+    assert_select "input[name=email_address][type=text][value]", false
+    assert_select "input[name=password][value]", false
+    assert_select "code", "jiwoo@example.com"
+    assert_select "code", "password123"
   end
 
   test "create with valid credentials" do
